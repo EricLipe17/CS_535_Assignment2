12be6e1c4ce0addda488a2e57a3a50b170301c82
Eric <elipe@goraft.tech>
2023-03-15 11:27:25 -0600

    - Update pom.xml and move sample spout to src dir

--- src/main/java/csu/cs535/TwitterSampleSpout.java
+++ src/main/java/csu/cs535/TwitterSampleSpout.java
@@ -1,9 +1,9 @@
-import org.apache.storm.Config;
+package csu.cs535;
+
 import org.apache.storm.spout.SpoutOutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichSpout;
-import org.apache.storm.utils.Utils;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
