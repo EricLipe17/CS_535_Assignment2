f079a4980f0c7ead30b928078cbfc870ab7c36b9
Eric <elipe@goraft.tech>
2023-03-26 20:15:52 -0600

    - Fixed error in LogBolt - Leaving debug code in main for now

--- src/main/java/csu/cs535/LogBolt.java
+++ src/main/java/csu/cs535/LogBolt.java
@@ -17,14 +17,13 @@ import java.util.concurrent.ConcurrentHashMap;
 class EntryComparator implements Comparator<Map.Entry<String, Long>> {
     @Override
     public int compare(Map.Entry<String, Long> e1, Map.Entry<String, Long> e2) {
-        return e1.getValue().compareTo(e2.getValue());
+        return e2.getValue().compareTo(e1.getValue());
     }
 }
 
 public class LogBolt extends BaseRichBolt {
     long last_log_time;
-    ConcurrentHashMap<String, Long> count_structure = new ConcurrentHashMap<>();
-    FileWriter fw;
+    ConcurrentHashMap<String, Long> hashtag_counts = new ConcurrentHashMap<>();
     BufferedWriter bw;
 
     OutputCollector collector;
@@ -39,8 +38,7 @@ public class LogBolt extends BaseRichBolt {
         this.collector = outputCollector;
         this.last_log_time = System.currentTimeMillis() / 1000L;
         try {
-            this.fw = new FileWriter("/tmp/hashtag_counts.log", false);
-            this.bw = new BufferedWriter(fw);
+            this.bw = new BufferedWriter(new FileWriter("/tmp/hashtag_counts.log", false));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -48,36 +46,43 @@ public class LogBolt extends BaseRichBolt {
 
     @Override
     public void execute(Tuple tuple) {
-        Map<String, ArrayList<Long>> structure = (Map<String, ArrayList<Long>>)tuple.getValue(0);
+        Map<String, ArrayList<Long>> count_structure = (ConcurrentHashMap<String, ArrayList<Long>>)tuple.getValue(0);
         Long count;
-        for (Map.Entry<String, ArrayList<Long>> entry : structure.entrySet()) {
+        for (Map.Entry<String, ArrayList<Long>> entry : count_structure.entrySet()) {
             final String hashtag = entry.getKey();
-            count = this.count_structure.get(hashtag);
+            count = this.hashtag_counts.get(hashtag);
             if (count == null) {
-                count = 0L;
+                count = entry.getValue().get(0);
             }
-            count++;
-            this.count_structure.put(hashtag, count);
+            else {
+                count += entry.getValue().get(0);
+            }
+            this.hashtag_counts.put(hashtag, count);
         }
         long curr_time = System.currentTimeMillis() / 1000L;
         if (curr_time - this.last_log_time >= 10) {
-            List<Map.Entry<String, Long>> entries = new LinkedList<>(this.count_structure.entrySet());
+            List<Map.Entry<String, Long>> entries = new LinkedList<>(this.hashtag_counts.entrySet());
             entries.sort(new EntryComparator());
 
-            ArrayList<String> topHashtags = new ArrayList<>(100);
             int loopVal = Math.min(entries.size(), 100);
+            ArrayList<String> topHashtags = new ArrayList<>(loopVal);
             for (int i = 0; i < loopVal; i++) {
                 topHashtags.add(entries.get(i).getKey());
             }
-            String log_line = curr_time + " " + StringUtils.join(topHashtags, ",");
+
+            StringBuilder log_line = new StringBuilder("<" + curr_time + ">");
+            for (String ht : topHashtags) {
+                log_line.append(String.format("<%s>", ht));
+            }
 
             try {
-                this.bw.write(log_line);
+                this.bw.write(log_line.toString());
+                this.bw.newLine();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
 
-            this.last_log_time = System.currentTimeMillis() / 1000L;
+            this.last_log_time = curr_time;
         }
     }
 
@@ -86,8 +91,6 @@ public class LogBolt extends BaseRichBolt {
         try {
             this.bw.flush();
             this.bw.close();
-
-            this.fw.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/csu/cs535/Main.java
+++ src/main/java/csu/cs535/Main.java
@@ -5,7 +5,16 @@ import org.apache.storm.StormSubmitter;
 import org.apache.storm.generated.AlreadyAliveException;
 import org.apache.storm.generated.AuthorizationException;
 import org.apache.storm.generated.InvalidTopologyException;
+import org.apache.storm.shade.org.apache.commons.lang.StringUtils;
 import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Values;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,5 +33,69 @@ public class Main {
         } catch (AlreadyAliveException | InvalidTopologyException | AuthorizationException e) {
             throw new RuntimeException(e);
         }
+//        long curr_time = System.currentTimeMillis() / 1000L;
+//        long current = 1L;
+//        ArrayList<Long> counts;
+//        ConcurrentHashMap<String, ArrayList<Long>> count_structure = new ConcurrentHashMap<>();
+//        String[] bucket = new String[]{"#CubaPorLaPaz", "#ballsqueeze", "#onlyfans", "#nsfwtwt", "#OylarTazminata", "#AWP23", "#Galatasaray", "#mypics", "#Splatoon", "#Splatoon3", "#NintendoSwitch", "#BuenosDias", "#poupettekenza", "#oriele", "#Shibarium", "#RYSFISK", "#RYSLANE", "#IMPORTANTE:", "#NowPlaying", "#RetroHitsCanada", "#mufc", "#Oscar", "#oscars95", "#RamCharan", "#RamCharanBossingOscars", "#OylarTazminata", "#Bills", "#london", "#jaihind", "#jaibhim", "#mizuruiweek2023", "#prsk_FA", "#gfvip", "#incorvassi", "#Eagles", "#HavadanZehirleniyoruz", "#bbb23", "#Bihar", "#viral", "#Trending", "#KeHuyQuan", "#BrendanFraser", "#dickrate", "#dm", "#nudes", "#cockrate", "#nsfw", "#bigdick", "#bwc", "#sext", "#porn", "#horny", "#dick", "#cum", "#rates", "#thickcock", "#thick", "#dickpic", "#bigcock", "#cocktribute", "#trade", "#pintoawards", "#cock", "#nsfwtwt", "#porn", "#fitness", "#youngcock", "#sext", "#nsfwt", "#dick", "#nudes", "#bbc", "#massage_in_downtown", "#massage_in_five", "#OylarTazminata", "#WWERaw", "#esenyurt", "#esenyurt", "#SULLYOON", "#NMIXX", "#Young_Dumb_Stupid", "#kerjaonline", "#WWERaw", "#esenyurt"};
+//        for (int i = 0; i < bucket.length; i++) {
+//            String hashtag = bucket[i];
+//            counts = count_structure.get(hashtag);
+//            if (counts == null) {
+//                counts = new ArrayList<>(2);
+//                counts.add(0L);
+//                counts.add(current - 1L);
+//            }
+//            long count = counts.get(0);
+//            count++;
+//            counts.set(0, count);
+//            count_structure.put(hashtag, counts);
+//        }
+//
+//        // Prune the data structure
+//        for (Map.Entry<String, ArrayList<Long>> entry : count_structure.entrySet()) {
+//            final String hashtag = entry.getKey();
+//            ArrayList<Long> freq_delta = entry.getValue();
+//            final long sum = freq_delta.get(0) + freq_delta.get(1);
+//
+//            if (sum <= current) {
+//                count_structure.remove(hashtag);
+//            }
+//        }
+//
+//        // Log bolt
+//        Long count;
+//        ConcurrentHashMap<String, Long> count_structure2 = new ConcurrentHashMap<>();
+//        for (Map.Entry<String, ArrayList<Long>> entry : count_structure.entrySet()) {
+//            final String hashtag = entry.getKey();
+//            count = count_structure2.get(hashtag);
+//            if (count == null) {
+//                count = entry.getValue().get(0);
+//            }
+//            else {
+//                count += entry.getValue().get(0);
+//            }
+//            count_structure2.put(hashtag, count);
+//        }
+//        long new_time = System.currentTimeMillis() / 1000L;
+//        long time = new_time - curr_time;
+//        if (time >= 10) {
+//            List<Map.Entry<String, Long>> entries = new LinkedList<>(count_structure2.entrySet());
+//            entries.sort(new EntryComparator());
+//
+//            int loopVal = Math.min(entries.size(), 100);
+//            ArrayList<String> topHashtags = new ArrayList<>(loopVal);
+//            for (int i = 0; i < loopVal; i++) {
+//                topHashtags.add(entries.get(i).getKey());
+//            }
+//
+//
+//            StringBuilder log_line = new StringBuilder("<" + curr_time + ">");
+//            for (String ht : topHashtags) {
+//                log_line.append(String.format("<%s>", ht));
+//            }
+////            String log_line = curr_time + " " + StringUtils.join(topHashtags, ",");
+//            System.out.println(log_line);
+//        }
     }
 }
